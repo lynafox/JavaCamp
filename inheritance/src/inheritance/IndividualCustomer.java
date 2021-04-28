df773619109e114de7a24689840249e659aaaeb8
lynafox <41112148+lynafox@users.noreply.github.com>
2021-04-29 01:00:48 +0300

    inheritance

---
+++ inheritance/src/inheritance/IndividualCustomer.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public class IndividualCustomer extends Customer{ //üst soy için extends yazılır. genişletilmiş anlamına gelir.
+	 String firstName;
+	 String lastName;
+	 String nationalIdentity;
+}
