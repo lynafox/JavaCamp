567a2939b48a7150bd839960979759e44f4fc9e2
lynafox <41112148+lynafox@users.noreply.github.com>
2021-04-29 01:01:34 +0300

    inheritance

--- inheritance/src/inheritance/Main.java
+++ inheritance/src/inheritance/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		IndividualCustomer anil = new IndividualCustomer();
-		anil.customerNumber ="13216";
+		anil.customerNumber ="1316";
 			
 		
 		
